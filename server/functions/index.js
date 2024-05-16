dd6e82d827112d03137f52900544fe114d7d28d9
Yifei Tao <u7272166@anu.edu.au>
2024-05-16 18:48:16 +1000

    Change to netlify

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,3 +1,4 @@
+const serverless = require('serverless-http');
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -5,7 +6,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,13 +17,10 @@ mongoose.connect(process.env.MONGODB_URI, {
   .then(() => console.log('MongoDB connected successfully.'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-// 引入并使用路由
-const urlCardsRouter = require('./routes/urlCards');
-const sidebarItemsRouter = require('./routes/sidebarItems');
+const urlCardsRouter = require('../routes/urlCards');
+const sidebarItemsRouter = require('../routes/sidebarItems');
 
 app.use('/api/urlCards', urlCardsRouter);
 app.use('/api/sidebarItems', sidebarItemsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports.handler = serverless(app);
